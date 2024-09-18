61cb31c177860ca47cfa4157208fe8c23b2d3659
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-18 11:57:00 +0200

    added and tested all endpoints for student controller.

---
+++ src/main/java/com/booleanuk/api/controller/StudentController.java
@@ -0,0 +1,70 @@
+package com.booleanuk.api.controller;
+
+import com.booleanuk.api.model.Student;
+import com.booleanuk.api.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+    private final StudentRepository studentRepository;
+    private static final String NOT_FOUND = "Student not found for ID: ";
+
+    @Autowired
+    public StudentController(StudentRepository repository) {
+        this.studentRepository = repository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAll() {
+        List<Student> studentList = this.studentRepository.findAll();
+        return ResponseEntity.ok(studentList);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Student> getById(@PathVariable int id) {
+        return ResponseEntity.ok(this.studentRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, NOT_FOUND + id)
+        ));
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> create(@RequestBody Student body) {
+        Student createdStudent = this.studentRepository.save(body);
+        return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Student> update(@PathVariable int id, @RequestBody Student body) {
+        Student student = this.studentRepository.findById(id).orElse(null);
+        if (student == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, NOT_FOUND + id);
+        }
+
+        student.setFirstName(body.getFirstName());
+        student.setLastName(body.getLastName());
+        student.setAverageGrade(body.getAverageGrade());
+        student.setStartDate(body.getStartDate());
+        student.setDateOfBirth(body.getDateOfBirth());
+        student.setCourseTitle(body.getCourseTitle());
+        this.studentRepository.save(student);
+
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Student> delete(@PathVariable int id) {
+        Student student = this.studentRepository.findById(id).orElse(null);
+        if (student == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, NOT_FOUND + id);
+        }
+        this.studentRepository.delete(student);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+}
