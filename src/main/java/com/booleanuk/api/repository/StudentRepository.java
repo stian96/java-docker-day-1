bf17035265414b9b79fee8e431b3e836d1417ac9
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-18 11:29:08 +0200

    added repository for student.

---
+++ src/main/java/com/booleanuk/api/repository/StudentRepository.java
@@ -0,0 +1,6 @@
+package com.booleanuk.api.repository;
+
+import com.booleanuk.api.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {}
